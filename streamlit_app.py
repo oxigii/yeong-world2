f95037325b2c94064ea47684b569ff033defb160
oxigii <162395947+oxigii@users.noreply.github.com>
2024-03-27 02:23:59 +0000

    oss0327

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
-import altair as alt
+import matplotlib.pyplot as plt
 
-# Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+# # Page title
+# st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
+# st.title('📊 Interactive Data Explorer')
 
+# st.sidebar.title('따라가기')
+# st.title("oss 0327")
+# st.color_picker('Choose your favorite color?')
+
+rand=np.random.normal(1,2,size=20)
+fig,ax=plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
